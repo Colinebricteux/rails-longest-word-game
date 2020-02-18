4f2b97c96bdfc06b296d9ecd1609c8da7eba00c6
Coline Bricteux <coline.bricteux@gmail.com>
2020-02-18 11:06:22 +0100

    checké la solution

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,17 +1,27 @@
 require "open-uri"
 
 class GamesController < ApplicationController
+
   def new
     @letters = Array.new(10) { ('A'..'Z').to_a.sample }
   end
 
   def score
-    if word
-      @answer = "<em>Congratulations!</em> #{word} is a valid English word!"
-    elsif word
-      @answer = "Sorry but #{word} does not seem to be a valid English word..."
-    elsif word
-      @answer = "Sorry but #{word} can't be built out of the letters"
-    end
+    @letters = params[:letters].split
+    @word = (params[:word] || "").upcase
+    @included = included?(@word, @letters)
+    @english_word = english_word?(@word)
+  end
+
+  private
+
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
+
+  def english_word?(word)
+    response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
   end
 end
